f71c1d877385c691ecb7837a3d82277631dd1e6d
Smit-Dharaiya <58761900+Smit-Dharaiya@users.noreply.github.com>
2020-07-12 11:37:58 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect('localhost','root','','Internship');
+$con=mysqli_connect('for-web-app.c6urukzyamge.us-east-2.rds.amazonaws.com','admin','12345678','internship');
 
     if(!$con){
       echo "error in connection";
